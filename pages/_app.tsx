2d772d3b505b38e7dc30da57dd5f49354b1b9cf0
ClarXo <cheonaru@gmail.com>
2022-05-28 22:54:01 +0900

    add amplify yml

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,4 @@
-import { NextPageContext } from 'next';
 import type { AppProps } from 'next/app';
-import { withSsrSession } from '../libs/server/withSsrSession';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
